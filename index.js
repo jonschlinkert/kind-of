670ea6b86c16bc263bb6d6c924ea46a03c7c43c3
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-19 01:04:07 -0500

    tweaks based on benchmarks

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+var toString = Object.prototype.toString;
 
 /**
  * Get the native `typeof` a value.
@@ -8,14 +8,12 @@
  */
 
 module.exports = function kindOf(val) {
-  if (val === null) {
-    return 'null';
-  }
-
   if (val === undefined) {
     return 'undefined';
   }
-
+  if (val === null) {
+    return 'null';
+  }
   if (typeof val !== 'object') {
     return typeof val;
   }
@@ -24,6 +22,17 @@ module.exports = function kindOf(val) {
     return 'array';
   }
 
-  return {}.toString.call(val)
-    .slice(8, -1).toLowerCase();
+  var type = toString.call(val);
+
+  if (val instanceof RegExp || type === '[object RegExp]') {
+    return 'regexp';
+  }
+  if (val instanceof Date || type === '[object Date]') {
+    return 'date';
+  }
+  if (type === '[object Function]') {
+    return 'function';
+  }
+
+  return type.slice(8, -1).toLowerCase();
 };
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * kind-of <https://github.com/jonschlinkert/kind-of>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT License
  */
 
