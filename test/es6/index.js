b37d7a02a57687478a5c8aa16c54cea81bfd6a50
Julian Jensen <jjdanois@gmail.com>
2021-03-05 10:58:10 -0500

    add support for 'bigint', fix deprecated Buffer constructor call

--- test/es6/index.js
+++ test/es6/index.js
@@ -135,5 +135,11 @@ module.exports = function() {
       var float64array = new Float64Array();
       assert.equal(kindOf(float64array), 'float64array');
     });
+
+    if ( typeof BigInt === 'function') {
+        it('should work for BigInt', function () {
+            assert.strictEqual(kindOf(42n), 'bigint');
+        });
+    }
   });
 };
